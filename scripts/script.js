ea7b71678e0d61e07c7200487559ab9e750316b9
mss <63112170+2seo@users.noreply.github.com>
2024-01-25 04:00:26 +0000

    🎨 프로필 이미지 레이아웃

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,7 @@
+const cover = document.querySelector(".cover");
 const profile = document.querySelector(".profile");
 
 window.addEventListener("scroll", ()=>{
     profile.style.bottom = window.scrollY+"px";
+    cover.style.backgroundPosition = `center ${-window.scrollY*0.7}px`;
 })
