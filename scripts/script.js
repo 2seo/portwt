a317043090256630e692faf990ae0ec2c385e60c
mss <63112170+2seo@users.noreply.github.com>
2024-01-25 06:53:13 +0000

    🎨 다크모드 구현

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,17 @@
 const cover = document.querySelector(".cover");
 const profile = document.querySelector(".profile");
+const toggle = document.querySelector("button");
 
-window.addEventListener("scroll", ()=>{
-    profile.style.bottom = window.scrollY+"px";
-    cover.style.backgroundPosition = `center ${-window.scrollY*0.7}px`;
-})
+window.addEventListener("scroll", () => {
+  profile.style.bottom = window.scrollY + "px";
+  cover.style.backgroundPosition = `center ${-window.scrollY * 0.7}px`;
+});
+
+let isOff = false;
+toggle.addEventListener("click", () => {
+  isOff = !isOff;
+  document.documentElement.setAttribute(
+    "color-theme",
+    isOff ? "light" : "dark"
+  );
+});
