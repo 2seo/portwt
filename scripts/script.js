76721bfc8f97fe85455a7da7a32183c19fc4b1ae
mss <63112170+2seo@users.noreply.github.com>
2024-01-26 06:56:17 +0000

    🎨 navbar 추가

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,14 @@
 const cover = document.querySelector(".cover");
 const profile = document.querySelector(".profile");
-const toggle = document.querySelector(".menu-toggle");
-const thank = document.querySelector(".like")
+const nav = document.querySelector("nav");
 
 window.addEventListener("scroll", () => {
   profile.style.bottom = window.scrollY + "px";
   cover.style.backgroundPosition = `center ${-window.scrollY /5}px`;
+  nav.style.display = window.scrollY>innerHeight ? "block" : "none";
 });
 
+const toggle = document.querySelector(".menu-toggle");
 let isOff = false;
 toggle.addEventListener("click", () => {
   isOff = !isOff;
@@ -17,6 +18,8 @@ toggle.addEventListener("click", () => {
   );
 });
 
+const thank = document.querySelector(".like")
 thank.addEventListener('click', () => {
     alert("Thank You! ( •̀ .̫ •́ )✧");
 })
+
