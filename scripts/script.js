06d20037951589aba7419e2a7173efa1c23490d6
mss <63112170+2seo@users.noreply.github.com>
2024-01-26 08:48:06 +0000

    🎨 navbar 스크립트 작성

--- scripts/script.js
+++ scripts/script.js
@@ -4,8 +4,8 @@ const nav = document.querySelector(".nav");
 
 window.addEventListener("scroll", () => {
   profile.style.bottom = window.scrollY + "px";
-  cover.style.backgroundPosition = `center ${-window.scrollY /5}px`;
-  nav.style.position = window.scrollY>innerHeight ? "fixed" : "unset";
+  cover.style.backgroundPosition = `center ${-window.scrollY / 5}px`;
+  nav.style.position = window.scrollY > innerHeight ? "fixed" : "unset";
 });
 
 const toggle = document.querySelector(".menu-toggle");
@@ -18,8 +18,21 @@ toggle.addEventListener("click", () => {
   );
 });
 
-const thank = document.querySelector(".like")
-thank.addEventListener('click', () => {
-    alert("Thank You! ( •̀ .̫ •́ )✧");
-})
+const thank = document.querySelector(".like");
+thank.addEventListener("click", () => {
+  alert("Thank You! ( •̀ .̫ •́ )✧");
+});
 
+const menus = document.querySelectorAll(".nav-inner span");
+menus.forEach((_, idx) => {
+  menus[idx].addEventListener("click", () => {
+    window.scrollTo(0, innerHeight);
+    for (i = 0; i < menus.length; i++) {
+      if (i === idx) {
+        menus[i].classList.add("selected");
+      } else {
+        menus[i].classList.remove("selected");
+      }
+    }
+  });
+});
