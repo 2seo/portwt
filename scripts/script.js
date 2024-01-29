8b181d360720650104d8bb0bde9bda89da8ba2ee
mss <63112170+2seo@users.noreply.github.com>
2024-01-29 04:38:47 +0000

    💄 메인 레이아웃 / 스크롤 액션 수정

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,13 @@
 const cover = document.querySelector(".cover");
 const profile = document.querySelector(".profile");
 const nav = document.querySelector(".nav");
+const main = document.querySelector(".main");
 
 window.addEventListener("scroll", () => {
   profile.style.bottom = window.scrollY + "px";
   cover.style.backgroundPosition = `center ${-window.scrollY / 5}px`;
   nav.style.position = window.scrollY > innerHeight ? "fixed" : "unset";
+  main.style.paddingTop = window.scrollY > innerHeight ? "50px" : 0;
 });
 
 const toggle = document.querySelector(".menu-toggle");
@@ -26,7 +28,11 @@ thank.addEventListener("click", () => {
 const menus = document.querySelectorAll(".nav-inner span");
 menus.forEach((_, idx) => {
   menus[idx].addEventListener("click", () => {
-    window.scrollTo(0, innerHeight);
+    window.scroll({
+      top: innerHeight*(idx+1),
+      left: 0,
+      behavior: "smooth",
+    });
     for (i = 0; i < menus.length; i++) {
       if (i === idx) {
         menus[i].classList.add("selected");
